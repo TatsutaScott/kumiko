9bf79cfc6f917f7872f2c068dec7221b16de6294
TatsutaScott <tatsuta.scott@gmail.com>
2024-01-21 21:24:34 +0900

    added download function

--- sketch.js
+++ sketch.js
@@ -5,3 +5,12 @@ const [canvas, ctx] = makeCanvas(width, height);
 
 ctx.fillStyle = "red";
 ctx.fillRect(0, 0, 1000, 1000);
+
+// download("test", canvas);
+
+function download(name, canvas) {
+  const link = document.createElement("a");
+  link.download = `${name}.png`;
+  link.href = canvas.toDataURL();
+  link.click();
+}
